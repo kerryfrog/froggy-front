55c96306671e877b73ed90837988ee7a28e5c0b7
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-10-07 20:29:45 +0900

    add axios credential

--- src/app/api/auth/auth.service.ts
+++ src/app/api/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import axios from "axios";
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 @Injectable({
   providedIn: "root",
