cd64dd7153476971e8251e39f8055b5274e96dfb
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-04 19:08:38 +0900

    change tab icon

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-tabs",
@@ -6,5 +6,36 @@ import { Component } from "@angular/core";
   styleUrls: ["tabs.page.scss"],
 })
 export class TabsPage {
+  @ViewChild("tabs", { static: false }) tabs: any;
   constructor() {}
+
+  public isMainIconSelected = false;
+  public isYarnIconSelected = false;
+  public isPatternIconSelected = false;
+  public isCommunityIconSelected = false;
+
+  async ngOnInit() {
+    //console.log(this.tabs.getSelected());
+  }
+
+  handleTabsChange(event) {
+    console.log(event.tab);
+    this.clearAll();
+    if (event.tab === "main") {
+      this.isMainIconSelected = true;
+    } else if (event.tab === "yarn") {
+      this.isYarnIconSelected = true;
+    } else if (event.tab === "pattern") {
+      this.isPatternIconSelected = true;
+    } else if (event.tab === "community") {
+      this.isCommunityIconSelected = true;
+    }
+  }
+
+  clearAll() {
+    this.isMainIconSelected = false;
+    this.isYarnIconSelected = false;
+    this.isPatternIconSelected = false;
+    this.isCommunityIconSelected = false;
+  }
 }
