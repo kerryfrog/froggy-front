e3c148e8b5ea0c1366dccd5177d3119b82e5ca7d
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-13 15:03:06 +0900

    review score 표시

--- src/app/components/review-list/review-list.component.ts
+++ src/app/components/review-list/review-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class ReviewListComponent implements OnInit {
   @Input() reviewList;
-
+  halfStar: string = "true";
   iconsArray: number[] = [];
   defaultIcon: string = "star-outline";
   halfIcon: string = "star-half";
