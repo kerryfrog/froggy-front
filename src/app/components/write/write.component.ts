e7cce461d42ae7ffedc5222f04ba15e3f11b3735
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-22 22:45:39 +0900

    사진 예전으로 되돌림 ..

--- src/app/components/write/write.component.ts
+++ src/app/components/write/write.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import {
-  NavController,
-  ModalController,
-  AlertController,
-} from "@ionic/angular";
+import { NavController, ModalController } from "@ionic/angular";
 import {
   FormGroup,
   FormBuilder,
@@ -33,15 +29,9 @@ export class WriteComponent implements OnInit {
   public image;
   public imageList = [];
 
-  //tmp
-  public isShowFileNames;
-  public limitCnt = 10;
-  public Images = [];
-  public imagesForDisplay = [];
-  public limitText = "";
   //작성 가능
   public isValidPost: boolean = false;
-  @ViewChild("fileInput", { static: false }) fileInput: any;
+
   @ViewChild("quillFile") quillFileRef: ElementRef;
   quillFile: any;
   quillEditorRef;
@@ -57,8 +47,8 @@ export class WriteComponent implements OnInit {
         ["link", "image"],
       ],
       handlers: {
-        image: async () => {
-          const fileInfos = await Promise.all(this.openUserPhotoLibrary());
+        image: (image) => {
+          this.customImageUpload(image);
         },
       },
     },
@@ -68,8 +58,7 @@ export class WriteComponent implements OnInit {
     public modalController: ModalController,
     public communityService: CommunityService,
     public userService: UserService,
-    public imageService: ImageService,
-    public alertController: AlertController
+    public imageService: ImageService
   ) {}
 
   ngOnInit() {}
@@ -99,11 +88,11 @@ export class WriteComponent implements OnInit {
     // this.meQuillRef = editorInstance;
     this.quillEditorRef = editorInstance;
   }
-  // customImageUpload(image: any) {
-  //   console.log(image);
-  //   /* Here we trigger a click action on the file input field, this will open a file chooser on a client computer */
-  //   this.quillFileRef.nativeElement.click();
-  // }
+  customImageUpload(image: any) {
+    console.log(image);
+    /* Here we trigger a click action on the file input field, this will open a file chooser on a client computer */
+    this.quillFileRef.nativeElement.click();
+  }
 
   async quillFileSelected(ev: any) {
     /* After the file is selected from the file chooser, we handle the upload process */
@@ -171,99 +160,6 @@ export class WriteComponent implements OnInit {
     }
   }
 
-  /* 이미지 관련 함수 */
-  // 이미지 버튼 클릭 핸들러 함수
-  setDefault() {
-    if (!this.limitCnt) this.limitCnt = 10;
-    if (!this.limitText)
-      this.limitText = "10개 이상의 사진을 첨부할 수 없습니다.";
-    if (!this.isShowFileNames) this.isShowFileNames = false;
-  }
-
-  async openUserPhotoLibrary(): Promise<void> {
-    this.fileInput.nativeElement.click();
-    const fileList = this.fileInput.nativeElement.files;
-    await this.getImageWeb(fileList);
-    // const imageUploadResult = await this.imageService.uploadSingleImage(
-    //   this.image
-    // );
-
-    // if (imageUploadResult.data.status === "Y") {
-    //   const inputList = imageUploadResult.data.imageUrlList;
-    //   const editor = this.quillEditorRef;
-    //   const range = editor.getSelection();
-    //   editor.insertEmbed(range + 1, "image", inputList[0]);
-    // } else {
-    //   alert(imageUploadResult.data.reason);
-    //   return;
-    // }
-
-    // console.log(this.Images);
-  }
-  async uploadImage(): Promise<void> {
-    console.log("promise의 세계");
-    return;
-    const imageUploadResult = await this.imageService.uploadSingleImage(
-      this.image
-    );
-  }
-  insertImageToEditor() {}
-
-  async getImageWeb(files) {
-    const fileExtension = /(.*?)\.(webp)$/;
-    for (let i = 0; i < files.length; i++) {
-      const fileInfo = files.item(i);
-      const imageName = fileInfo.name;
-      const imageType = fileInfo.type;
-      const extension = imageType.split("/")[1];
-      if (files.item(i).name.match(fileExtension)) {
-        const alert = await this.alertController.create({
-          header: "",
-          subHeader: "",
-          message: ".webp 형식의 이미지 파일은 업로드 할 수 없습니다.",
-          buttons: [{ text: "확인" }],
-        });
-        await alert.present();
-        return;
-      }
-
-      const reader = new FileReader();
-
-      reader.readAsDataURL(files.item(i));
-
-      reader.onload = async () => {
-        const imageData = {
-          fileName: undefined,
-          fileFormat: undefined,
-          fileBlob: undefined,
-        };
-        const displayImageData = {
-          imageName: undefined,
-          image: undefined,
-        };
-        displayImageData.imageName = imageName;
-        displayImageData.image = reader.result;
-        imageData.fileBlob = fileInfo;
-        imageData.fileFormat = extension;
-        imageData.fileName = imageName;
-        this.Images.push(imageData);
-        console.log("end onload");
-      };
-
-      reader.onloadend = async () => {
-        try {
-          alert("image");
-          const imageUploadResult =
-            await this.imageService.uploadSingleImageFile(this.Images[0]);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    }
-
-    this.fileInput.nativeElement.value = "";
-  }
-
   checkIsValid(): boolean {
     const title = this.title.replace(/(\s*)/g, "");
     const category = this.category;
@@ -279,19 +175,9 @@ export class WriteComponent implements OnInit {
     }
   }
 
-  // emitImgSrcChange() {
-  //   this.imgSrcListChange.emit(this.Images);
-  // }
-
   async setUserSyncWithServer() {
     await this.userService.deleteUser();
-    const alert = await this.alertController.create({
-      header: "안내",
-      subHeader: "",
-      message: "다시 로그인 해주세요",
-      buttons: [{ text: "확인" }],
-    });
-    await alert.present();
+    alert("다시 로그인 해 주세요");
   }
   goBackWithSuccess() {
     this.modalController.dismiss({
@@ -303,24 +189,26 @@ export class WriteComponent implements OnInit {
       dismissed: false,
     });
   }
-  b64toBlob = async (b64Data, contentType = "", sliceSize = 512) => {
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
 
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
+  // // base64를 blob으로 변경하는 함수
+  // b64toBlob = async (b64Data, contentType = "", sliceSize = 512) => {
+  //   const byteCharacters = atob(b64Data);
+  //   const byteArrays = [];
 
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
+  //   for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+  //     const slice = byteCharacters.slice(offset, offset + sliceSize);
 
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
+  //     const byteNumbers = new Array(slice.length);
+  //     for (let i = 0; i < slice.length; i++) {
+  //       byteNumbers[i] = slice.charCodeAt(i);
+  //     }
 
-    const blob = new Blob(byteArrays, { type: contentType });
+  //     const byteArray = new Uint8Array(byteNumbers);
+  //     byteArrays.push(byteArray);
+  //   }
 
-    return blob;
-  };
+  //   const blob = new Blob(byteArrays, { type: contentType });
+
+  //   return blob;
+  // };
 }
