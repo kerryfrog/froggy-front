f93f1ad529e2db07e1bfc78f05f14f358c9bdc12
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-22 18:26:08 +0900

    serach bar 로직 변경

--- src/app/pages/pattern-search/pattern-search.page.ts
+++ src/app/pages/pattern-search/pattern-search.page.ts
@@ -9,7 +9,8 @@ import { LoadingController, NavController } from "@ionic/angular";
 })
 export class PatternSearchPage implements OnInit {
   public patternList = [];
-  public results;
+  public value;
+  public isWaiting = false;
   constructor(
     public patternService: PatternService,
     public navController: NavController
@@ -18,34 +19,25 @@ export class PatternSearchPage implements OnInit {
   ngOnInit() {}
 
   async handleChange(event) {
-    this.results = event.target.value;
-    const { data } = await this.patternService.getPatternSearchList(
-      this.results
-    );
-
-    this.patternList = data["searchList"];
-    console.log(this.patternList);
+    this.value = event.target.value;
   }
-
-  checkIsPatternFavorite(favoritePatternList) {
-    if (!favoritePatternList) return;
-    this.patternList = this.patternList.map((pattern) => {
-      const patterns = { ...pattern };
-      const isFavorite = favoritePatternList.includes(pattern.id);
-      if (isFavorite) {
-        patterns.isFavorite = true;
-      } else {
-        patterns.isFavorite = false;
-      }
-      return patterns;
-    });
+  async search(event) {
+    this.patternList = [];
+    const value = this.value.replace(/(\s*)/g, "");
+    if (value.length > 0) {
+      this.isWaiting = true;
+      const { data } = await this.patternService.getPatternSearchList(
+        this.value
+      );
+      this.patternList = data["searchList"];
+      this.isWaiting = false;
+    }
   }
 
   async goPatternDetailPage(pattern) {
     const props: NavigationExtras = {
       state: {
         pattern,
-        previous: "tabs/pattern/search",
       },
     };
     this.navController.navigateForward(`/tabs/pattern/${pattern.id}`, props);
