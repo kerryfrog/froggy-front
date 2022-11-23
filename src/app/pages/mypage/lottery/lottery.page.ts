f253a8f5292b984085591b90a5f3776d8cd9a4e0
synodical <lena0323park@naver.com>
2022-11-23 22:50:44 +0900

    랜덤 등수 반환

--- src/app/pages/mypage/lottery/lottery.page.ts
+++ src/app/pages/mypage/lottery/lottery.page.ts
@@ -8,9 +8,26 @@ import { UserService } from "src/app/services/user.service";
 export class LotteryPage implements OnInit {
   constructor(public userService: UserService) {}
   public user;
+  public curIcon = "heart";
+  public arr = [1, 2, 2, 3, 3, 3];
+  public rating = -1;
   ngOnInit() {}
+  // 확률: 1/6, 2/6, 3/6
   async ionViewDidEnter() {
     await this.getUser();
+  }
+  async onChangeSegment(event) {
+    this.curIcon = event.detail.value;
+  }
+  async onSelectSegment() {
+    const shuffledArr = await this.shuffleArray(this.arr);
+    console.log(shuffledArr, shuffledArr[this.curIcon]);
+    this.rating = shuffledArr[this.curIcon];
+  }
+  async shuffleArray(array) {
+    array.sort(() => Math.random() - 0.5);
+    //console.log(array);
+    return array;
   }
 
   async getUser() {
